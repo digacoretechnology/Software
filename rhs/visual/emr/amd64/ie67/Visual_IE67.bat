a6d0040f003340ebd138857abecf74989b5c1f4b
aryehgreenspan <agreenspan@digacore.com>
2024-01-15 17:04:23 -0500

    Adding Visual RHS Folder

---
+++ rhs/visual/emr/amd64/ie67/Visual_IE67.bat
@@ -0,0 +1,42 @@
+@echo off
+echo starting Visual.
+cls
+echo starting Visual...
+cls
+echo starting Visual...
+cls
+echo starting Visual...
+@echo off
+
+setlocal ENABLEEXTENSIONS
+set KEY_NAME="HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+set VALUE_NAME=1201
+
+FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+FOR /F "usebackq skip=4 tokens=1-3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+
+if "%ValueValue%" == "0x0" goto checkietoedge
+regedit /s C:\rhs\visual\emr\iesecurity.reg
+
+
+
+
+:checkietoedge
+
+REG QUERY "HKCU\Software\Microsoft\Edge\IEToEdge" /v "RedirectionMode" | Find "0x0"
+IF %ERRORLEVEL% == 0 goto startvisual
+
+
+regedit /s C:\rhs\visual\emr\iesecurity.reg
+
+:startvisual
+"C:\Program Files (x86)\Internet Explorer\iexplore.exe" -new https://visualemr.rhsvisual2.com/rhsweb/login.asp?ws=%computername%
+
---
+++ rhs/visual/emr/x86/edge/Visual_edge.bat
@@ -0,0 +1,38 @@
+@echo off
+cls
+echo starting Visual...
+@echo off
+
+setlocal ENABLEEXTENSIONS
+set KEY_NAME="HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+set VALUE_NAME=1201
+
+FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+FOR /F "usebackq skip=4 tokens=1-3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+
+if "%ValueValue%" == "0x0" goto startvisual
+regedit /s C:\rhs\visual\emr\edgesecurity.reg
+
+
+
+
+rem :checkietoedge
+
+rem REG QUERY "HKCU\Software\Microsoft\Edge\IEToEdge" /v "RedirectionMode" | Find "0x0"
+rem IF %ERRORLEVEL% == 0 goto startvisual
+
+
+rem regedit /s C:\rhs\visual\emr\edgesecurity.reg
+
+
+:startvisual
+"C:\Program Files\Microsoft\Edge\Application\msedge.exe" --new-window https://visualemr.rhsvisual2.com/rhsweb/login.asp?ws=%computername% 
+
